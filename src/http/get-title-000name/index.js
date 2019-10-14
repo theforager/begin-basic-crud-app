1ae996c9cf2540e396f0f3e3bff945e8c4daecba
Patrick <patrickjmanion@gmail.com>
2019-10-14 15:54:02 -0500

    Implement basic title response

--- src/http/get-title-000name/index.js
+++ src/http/get-title-000name/index.js
@@ -29,10 +29,13 @@ let html = `
 // HTTP function
 exports.handler = async function http(req) {
   console.log(req)
+  let body = parseBody(req) // Pass the entire request object
+  let name = body.name  // 'Patrick'
+  let response = name + ' is a boss'
   return {
     headers: {
       'content-type': 'text/html; charset=utf8'
     },
-    body: html
+    body: response
   }
 }
